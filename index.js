3dd69fb2b4c29ee9c1c00a893cb4473003d49c17
Code A Program <75136330+Sridhar-C-25@users.noreply.github.com>
2023-03-12 18:45:16 +0530

    deploy fullapp

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const dotenv = require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.send("Hello World! change");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./frontend/dist")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "./", "frontend", "dist", "index.html")
+    )
+  );
+}
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Example app listening on port ${process.env.PORT || 3000}!`);

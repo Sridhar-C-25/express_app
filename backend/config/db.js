dc8bd68ffce3c5c45740f1fafa58db3426f168a5
Code A Program <75136330+Sridhar-C-25@users.noreply.github.com>
2023-03-12 19:42:08 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,6 +5,8 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      ssl: true,
+      maxPoolSize: 10,
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {

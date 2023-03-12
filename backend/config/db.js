6c3d6ff2c130cb286b1ddf122079e87de0c036c4
Code A Program <75136330+Sridhar-C-25@users.noreply.github.com>
2023-03-12 19:49:36 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -11,7 +11,6 @@ const connectDB = async () => {
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.log(error);
-    process.exit(1);
   }
 };
 

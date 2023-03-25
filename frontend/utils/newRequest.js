d198700772f7ceb8d44558775d1c54250fc1c918
sridhar <iamsmartsri@gmail.com>
2023-03-26 01:14:13 +0530

    api base url

--- frontend/utils/newRequest.js
+++ frontend/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const newRequest = axios.create({
-  baseURL: "http://192.168.1.100:3000/api",
-  // baseURL: "/api",
+  // baseURL: "http://192.168.1.100:3000/api",
+  baseURL: "/api",
   withCredentials: true,
 });

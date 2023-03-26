c1b30c28d1e7e24e062858b715158f981efa1126
sridhar <iamsmartsri@gmail.com>
2023-03-26 15:09:37 +0530

    ts

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,15 +4,12 @@ const sendEmail = async (email, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
       service: "gmail",
-      secure: process.env.NODE_ENV === "production",
       auth: {
         user: process.env.USER_EMAIL_ID,
         pass: process.env.USER_EMAIL_PASS,
       },
-      tls: {
-        rejectUnauthorized: false,
-      },
     });
+
     const res = await transporter.sendMail({
       from: "iamsmartsri@gmail.com",
       to: email,

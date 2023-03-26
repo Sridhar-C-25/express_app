9375eafd74bd59a1c82daf4159af494535232d60
sridhar <iamsmartsri@gmail.com>
2023-03-26 11:57:18 +0530

    html file

--- frontend/utils/newRequest.js
+++ frontend/utils/newRequest.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const newRequest = axios.create({
-  baseURL: import.meta.env.VITE_APP_API_BASE_URL,
+  baseURL:
+    process.env.NODE_ENV == "production"
+      ? import.meta.env.VITE_APP_API_BASE_URL_PROD
+      : import.meta.env.VITE_APP_API_BASE_URL_DEV,
   withCredentials: true,
 });

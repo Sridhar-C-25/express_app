857a75fd8aed3816683927c6cca961381ace01e0
sridhar <iamsmartsri@gmail.com>
2023-03-26 13:41:00 +0530

    mail

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,17 +2,24 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (email, subject, text) => {
   try {
+    // const transporter = nodemailer.createTransport({
+    //   service: "gmail",
+    //   secure: process.env.NODE_ENV === "production",
+    //   auth: {
+    //     user: process.env.USER_EMAIL_ID,
+    //     pass: process.env.USER_EMAIL_PASS,
+    //   },
+    // });
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      secure: process.env.NODE_ENV === "production",
+      host: "smtp.ethereal.email",
+      port: 587,
       auth: {
-        user: process.env.USER_EMAIL_ID,
-        pass: process.env.USER_EMAIL_PASS,
+        user: "oleta.bins@ethereal.email",
+        pass: "RJnSsAeYv3KFAuKTmE",
       },
     });
-
     const res = await transporter.sendMail({
-      from: "codeaprogram@gmail.com",
+      from: "iamsmartsri@gmail.com",
       to: email,
       subject: subject,
       text: text,
